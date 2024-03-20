801349b4408298ddb0b01314c1bcea3fd410d976
Peter Kamau <kamaupeter343@gmail.com>
2024-03-21 00:34:11 +0300

    Made some changes to some of the components

--- components/forms/JobApplicationForm.tsx
+++ components/forms/JobApplicationForm.tsx
@@ -1,117 +1,138 @@
 "use client";
 
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, FieldProps } from "formik";
 import * as Yup from "yup";
 import { Button } from "@mui/material";
+import ReactQuill from "react-quill";
+import { createJobApplication } from "@/server";
+import { useParams, useRouter } from "next/navigation";
 
 interface FormValues {
-  name: string;
-  email: string;
-
-  resume: null;
+	name: string;
+	email: string;
+	cover_letter: string;
+	resume: string;
 }
 
 const validationSchema = Yup.object({
-  email: Yup.string().email("Invalid email address").required("Required"),
-  name : Yup.string().required("Name is required"),
-  resume: Yup.mixed().required("Resume is required"),
+	email: Yup.string().email("Invalid email address").required("Required"),
+	name: Yup.string().required("Name is required"),
+	resume: Yup.mixed().required("Resume is required"),
 });
 
 const initialValues = {
-  name: "",
-  email: "",
-  resume: null,
+	name: "",
+	email: "",
+	resume: "",
+	cover_letter: "",
 };
 
 export function JobApplicationForm() {
-  const handleSubmit = () => {
-    console.log("job application submitted sucessfully");
-  };
+	const params = useParams<{ id: string }>();
+	const handleSubmit = async (values: FormValues) => {
+		const jobApp = await createJobApplication(params.id, values);
+		console.log(jobApp);
+	};
 
-  return (
-    <div className="relative flex justify-center items-center flex-col text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
-      <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-        Apply for Job.title
-      </h4>
+	return (
+		<div className='relative flex justify-center items-center flex-col text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border'>
+			<h4 className='block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900'>
+				Apply for Job.title
+			</h4>
 
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={(values: FormValues, { setSubmitting }) => {
-          console.log(values);
-          handleSubmit();
-          setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting, values, errors, touched }) => (
-          <Form
-            encType="multipart/form-data"
-            className="max-w-screen-lg mt-8 mb-2 w-80 sm:w-96  "
-          >
-            <div className="flex flex-col gap-6 mb-1">
-              <h6 className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
-                Your Name
-              </h6>
-              <div className="relative h-11 w-full min-w-[200px]">
-                <Field
-                  type="text"
-                  name="name"
-                  placeholder="john doe"
-                  className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500 text-sm mt-1"
-                />
-              </div>
-              <h6 className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
-                Your Email
-              </h6>
-              <div className="relative h-11 w-full min-w-[200px]">
-                <Field
-                  type="email"
-                  name="email"
-                  placeholder="name@mail.com"
-                  className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500 text-sm mt-1"
-                />
-              </div>
-              <h6 className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
-                Your Resume
-              </h6>
-              <div className="relative h-11 w-full min-w-[200px]">
-                <Field
-                  type="file"
-                  name="resume"
-                  placeholder="resume"
-                  className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                />
-                <ErrorMessage
-                  name="resume"
-                  component="div"
-                  className="text-red-500 text-sm mt-1"
-                />
-              </div>
-            </div>
+			<Formik
+				initialValues={initialValues}
+				validationSchema={validationSchema}
+				onSubmit={(values: FormValues, { setSubmitting }) => {
+					console.log(values);
+					handleSubmit(values);
+					setSubmitting(false);
+				}}>
+				{({ isSubmitting, values, errors, touched }) => (
+					<Form
+						encType='multipart/form-data'
+						className='max-w-screen-lg mt-8 mb-2 w-80 sm:w-96  '>
+						<div className='flex flex-col gap-6 mb-1'>
+							<h6 className='block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900'>
+								Your Name
+							</h6>
+							<div className='relative h-11 w-full min-w-[300px]'>
+								<Field
+									type='text'
+									name='name'
+									placeholder='john doe'
+									className='peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'
+								/>
+								<ErrorMessage
+									name='email'
+									component='div'
+									className='text-red-500 text-sm mt-1'
+								/>
+							</div>
+							<h6 className='block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900'>
+								Your Email
+							</h6>
+							<div className='relative h-11 w-full min-w-[300px]'>
+								<Field
+									type='email'
+									name='email'
+									placeholder='name@mail.com'
+									className='peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'
+								/>
+								<ErrorMessage
+									name='email'
+									component='div'
+									className='text-red-500 text-sm mt-1'
+								/>
+							</div>
+							<h6 className='block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900'>
+								Your Resume
+							</h6>
+							<div className='relative h-11 w-full min-w-[300px]'>
+								<Field
+									type='file'
+									name='resume'
+									placeholder='resume'
+									className='peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50'
+								/>
+								<ErrorMessage
+									name='resume'
+									component='div'
+									className='text-red-500 text-sm mt-1'
+								/>
+							</div>
+							<h6 className='block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900'>
+								Your Cover Letter
+							</h6>
+							<div className='relative h-full w-full min-w-[300px]'>
+								<Field name='cover_letter'>
+									{({ field }: FieldProps) => (
+										<ReactQuill
+											value={field.value}
+											onChange={field.onChange(field.name)}
+										/>
+									)}
+								</Field>
+								<ErrorMessage
+									name='cover_letter'
+									component='div'
+									className='text-red-500 text-sm mt-1'
+								/>
+							</div>
+						</div>
 
-            <Button
-              variant="outlined"
-              color="primary"
-              type="submit"
-              disabled={isSubmitting}
-              className="mt-6 w-full"
-            >
-              {isSubmitting ? "Submitting..." : "Submit"}
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+						<Button
+							variant='outlined'
+							color='primary'
+							type='submit'
+							disabled={isSubmitting}
+							className='mt-6 w-full'>
+							{isSubmitting ? "Submitting..." : "Submit"}
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</div>
+	);
 }
--- components/job-detail/JobDetails.tsx
+++ components/job-detail/JobDetails.tsx
@@ -1,56 +1,80 @@
+"use client";
 import { getJob } from "@/server";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { JobDescription } from ".";
-import Link from "next/link";
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { JobStatus, JobType } from "@prisma/client";
 
-export async function JobDetails({ params }: { params: { id: string } }) {
-  const { id } = params;
-  const job = await getJob(id);
+interface JobApplication {
+	id: string;
+	title: string;
+	description: string;
+	salary_compensation: string | null;
+	location: string | null;
+	organization: string | null;
+	role: string | null;
+	status: JobStatus;
+	job_type: JobType | null;
+	vacancies: number | null;
+}
 
-  if (!job) return null;
+export function JobDetails({ params }: { params: { id: string } }) {
+	const [job, setJob] = useState<JobApplication | null>(null);
+	const { id } = params;
+	const router = useRouter();
+	useEffect(() => {
+		const fetchJob = async () => {
+			const job = await getJob(id);
+			setJob(job);
+		};
+		fetchJob();
+	}, [id]);
 
-  return (
-    <div className="overflow-y-auto overflow-x-hidden justify-center items-center max-h-full w-full">
-      <div className="relative p-4 max-h-full max-w-4xl mx-auto">
-        <div className="relative bg-white rounded-lg shadow w-full">
-          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t w-full">
-            <h3 className="text-xl font-semibold text-gray-900">
-              Job Decription
-            </h3>
-          </div>
-          <div className="p-4 md:p-5 space-y-4">
-            <p className="text-base leading-relaxed text-gray-600">
-              Company/Organiation: <span>---</span>
-            </p>
-            <p className="text-base leading-relaxed text-gray-600">
-              Job Title: <span>{job.title}</span>
-            </p>
-            <p className="text-base leading-relaxed text-gray-600">
-              Job Salary:{" "}
-              <span>{job.salary_compensation ?? "Not Provied"}</span>
-            </p>
-            <p className="text-base leading-relaxed text-gray-600">
-              Job Location: <span>{job.organization ?? "No Organization"}</span>
-            </p>
+	if (!job) return null;
 
-            <p className="text-base leading-relaxed text-gray-600">
-              Job Type: <span>{job.job_type}</span>
-            </p>
-            <p>
-              <JobDescription desc={job.description}/>
-            </p>
-          </div>
-          <div className="flex justify-end p-4 md:p-5 border-t border-gray-200 rounded-b">
-            <Link
-            href={'/job-application'}
-              type="button"
-              className="text-white bg-primary font-medium rounded-lg  px-5 py-2.5 text-center"
-            >
-              Apply Now
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='overflow-y-auto overflow-x-hidden justify-center items-center max-h-full w-full'>
+			<div className='relative p-4 max-h-full max-w-4xl mx-auto'>
+				<div className='relative bg-white rounded-lg shadow w-full'>
+					<div className='flex items-center justify-between p-4 md:p-5 border-b rounded-t w-full'>
+						<h3 className='text-xl font-semibold text-gray-900'>
+							Job Decription
+						</h3>
+					</div>
+					<div className='p-4 md:p-5 space-y-4'>
+						<p className='text-base leading-relaxed text-gray-600'>
+							Company/Organiation: <span>---</span>
+						</p>
+						<p className='text-base leading-relaxed text-gray-600'>
+							Job Title: <span>{job.title}</span>
+						</p>
+						<p className='text-base leading-relaxed text-gray-600'>
+							Job Salary:{" "}
+							<span>{job.salary_compensation ?? "Not Provied"}</span>
+						</p>
+						<p className='text-base leading-relaxed text-gray-600'>
+							Job Location: <span>{job.organization ?? "No Organization"}</span>
+						</p>
+
+						<p className='text-base leading-relaxed text-gray-600'>
+							Job Type: <span>{job.job_type}</span>
+						</p>
+						<p>
+							<JobDescription desc={job.description} />
+						</p>
+					</div>
+					<div className='flex justify-end p-4 md:p-5 border-t border-gray-200 rounded-b'>
+						<Button
+							onClick={() => router.push(`${id}/job-application`)}
+							type='button'
+							variant='contained'
+							className='text-white bg-primary font-medium rounded-lg  px-5 py-2.5 text-center'>
+							Apply Now
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
