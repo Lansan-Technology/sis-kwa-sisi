777a358dac44abc94922587e5c8050386838aa1c
lanteiraph <santamulantei8@gmail.com>
2024-03-20 13:04:56 +0300

    Add landing page

---
+++ app/jobs/page.tsx
@@ -0,0 +1,215 @@
+import React from "react";
+
+export default async function page() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <section className="flex gap-2 items-center flex-wrap justify-center">
+        {[1, 2, 3, 4].map((num) => {
+          return <JobListingCard key={num + "job"} />;
+        })}
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 ">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <a href="/" className="flex items-center">
+            <span className="self-center text-xl font-semibold whitespace-nowrap">
+              Sisi Kwa Sisi
+            </span>
+          </a>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl- rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+function HeroSection() {
+  return (
+    <section className="bg-white">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <a
+          href="#"
+          className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-ful hover:bg-gray-20 rounded-full"
+          role="alert"
+        >
+          <span className="text-sm font-medium  bg-primary-600 rounded-full px-4 py-1.5 mr-3 border border-primary">
+            Get access to job around you!
+          </span>
+          <svg
+            className="ml-2 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6x">
+          We invest in the your potential
+        </h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+          Unlock Your Potential: Discover Your Dream Job with Sisi kwa Sisi! 🚀
+          #CareerGoals #JobSearchMadeEasy
+        </p>
+        <form className="max-w-md mx-auto">
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only"
+          >
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-50"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+              placeholder="Find jobs..."
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute end-2.5 bottom-2.5 bg-primary hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="bg-white">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024
+          <a href="/" className="hover:underline">
+            {" "}
+            Sisi Kwa Sisi
+          </a>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              Licensing
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+function JobListingCard() {
+  return (
+    <div className="m-5">
+      <div className="group mx-2 mt-10 grid max-w-screen-md grid-cols-12 space-x-8 overflow-hidden rounded-lg border py-8 text-gray-700 shadow transition hover:shadow-lg sm:mx-auto">
+        <a
+          href="#"
+          className="order-2 col-span-1 mt-4 -ml-14 text-left text-gray-600 hover:text-gray-700 sm:-order-1 sm:ml-4"
+        >
+          <div className="group relative h-16 w-16 overflow-hidden rounded-lg">
+            <img
+              src="/images/EC25KRDBo-K3w8GexNHSE.png"
+              alt=""
+              className="h-full w-full object-cover text-gray-700"
+            />
+          </div>
+        </a>
+        <div className="col-span-11 flex flex-col pr-8 text-left sm:pl-4">
+          <h3 className="text-sm text-gray-600">Invision</h3>
+          <a
+            href="#"
+            className="mb-3 overflow-hidden pr-7 text-lg font-semibold sm:text-xl"
+          >
+            {" "}
+            Sr. Frontend Engineer{" "}
+          </a>
+          <p className="overflow-hidden pr-7 text-sm">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna .
+          </p>
+
+          <div className="mt-5 flex flex-col space-y-3 text-sm font-medium text-gray-500 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-2">
+            <div className="">
+              Experience:
+              <span className="ml-2 mr-3 rounded-full bg-green-100 px-2 py-0.5 text-green-900">
+                {" "}
+                2 Years{" "}
+              </span>
+            </div>
+            <div className="">
+              Salary:
+              <span className="ml-2 mr-3 rounded-full bg-blue-100 px-2 py-0.5 text-blue-900">
+                180-250k
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#7F27FF',
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
