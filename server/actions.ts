2cf1921a66578005fcf3a0d91dad2a238926054c
Peter Kamau <kamaupeter343@gmail.com>
2024-03-20 12:40:15 +0300

    added the get all Jobs,create Job Posting, and create Job Application

---
+++ server/actions.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { Job, JobApplication } from "./interfaces/interfaces";
+
+export async function createEmployer() {}
+
+export async function createJobsSeeker() {}
+
+export async function getAllJobs() {
+	return await prisma.job.findMany();
+}
+
+export async function getJob(id: string) {
+	return await prisma.job.findUnique({
+		where: {
+			data: {
+				id: id,
+			},
+		},
+	});
+}
+
+export async function createJobPosting(data: Job) {
+	return await prisma.job.create({
+		data: {},
+	});
+}
+
+export async function createJobApplication(id: string, data: JobApplication) {
+	const job = await getJob(id);
+
+	return await prisma.job_application({
+		data: {
+			jobId: job.id,
+		},
+	});
+}
