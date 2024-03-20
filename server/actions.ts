9f6b27b3511c3ba9cc97d6e1e969e26431eeff20
Peter Kamau <kamaupeter343@gmail.com>
2024-03-21 00:34:28 +0300

    updated the actions and interfaces

--- server/actions.ts
+++ server/actions.ts
@@ -1,32 +1,37 @@
 "use server";
 
 import { PrismaClient } from "@prisma/client";
-import { Employer, Job, JobApplication, JobSeeker } from "./interfaces/interfaces";
+import {
+	Employer,
+	Job,
+	JobApplication,
+	JobSeeker,
+} from "./interfaces/interfaces";
 
 const prisma = new PrismaClient();
 
 export async function createEmployer(data: Employer) {
-	const createdEmployer = await prisma.employer.create({data})
+	const createdEmployer = await prisma.employer.create({ data });
 	return createdEmployer;
 }
 
 export async function createJobsSeeker(data: JobSeeker) {
-	const jobsSeeker = await prisma.job_seeker.create({data: {
-		names: data.names,  
-        email: data.email,
-		location:  data.location, 
-		phone_number:  data.phone_number,
-		qualification: data.qualification,
-		course: data.course,
-		experience: data.experience,
-		cv: data.cv,
-		gender: data.gender,
-		photo: data.photo,
-		certification: data.certification
-
-	}});
+	const jobsSeeker = await prisma.job_seeker.create({
+		data: {
+			names: data.names,
+			email: data.email,
+			location: data.location,
+			phone_number: data.phone_number,
+			qualification: data.qualification,
+			course: data.course,
+			experience: data.experience,
+			cv: data.cv,
+			gender: data.gender,
+			photo: data.photo,
+			certification: data.certification,
+		},
+	});
 	return jobsSeeker;
-
 }
 
 export async function getAllJobs() {
@@ -36,19 +41,18 @@ export async function getAllJobs() {
 export async function getJob(id: string) {
 	return await prisma.job.findUnique({
 		where: {
-				id: id,
+			id: id,
 		},
 	});
 }
 
-export async function getJobSeeker(id: string) {
+export async function getJobSeeker(email: string) {
 	return await prisma.job_seeker.findUnique({
 		where: {
-			id: id
-		}
-	}
-	)
-} 
+			email: email,
+		},
+	});
+}
 
 export async function createJobPosting(data: Job) {
 	return await prisma.job.create({
@@ -59,19 +63,18 @@ export async function createJobPosting(data: Job) {
 	});
 }
 
-export async function createJobApplication(job_seekerid: string, id: string, data: JobApplication) {
+export async function createJobApplication(id: string, data: JobApplication) {
 	const job = await getJob(id);
-	if(!job) throw  new Error("No such job");
+	if (!job) throw new Error("No such job");
 
-	const job_seeker = await getJobSeeker(id);
-	if(!job_seeker) throw  new Error("Invalid Job Seeker ID");
+	const job_seeker = await getJobSeeker(data.email);
+	if (!job_seeker) throw new Error("Invalid Job Seeker ID");
 	return await prisma.job_application.create({
 		data: {
 			job_seekerid: job_seeker.id,
 			jobId: job.id,
 			cover_letter: data.cover_letter,
-			cv:  data.cv,
-			date: data.date
+			cv: data.resume,
 		},
 	});
 }
