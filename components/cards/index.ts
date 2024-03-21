861aaa1fb3cc2e2aa378ce090a6f5b5f7004a9c3
habertrogena <habertorogy12@gmail.com>
2024-03-21 13:11:03 +0300

    creates a component that displays all the applied jobs

--- components/cards/index.ts
+++ components/cards/index.ts
@@ -1,1 +1,2 @@
-export * from './JobListingCard'
+export * from './JobListingCard';
+export * from './MyApplication';
--- components/sections/Jobs.tsx
+++ components/sections/Jobs.tsx
@@ -4,6 +4,7 @@ import { JobListingCard } from "..";
 
 export async function Jobs({ searchTitle }: { searchTitle?: string }) {
   const jobs = await findJobsByTitleOrAll(searchTitle, 4);
+ 
 
   return (
     <section className="flex gap-2 items-center flex-wrap justify-center">
